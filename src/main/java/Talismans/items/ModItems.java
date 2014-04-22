802aed9eaf42432d3e7b9ed6505047eead5ae1df
Travis Nicholls <thegigabit101@gmail.com>
2014-04-22 23:21:54 +0100

    Copy Talismans

--- src/main/java/Talismans/items/ModItems.java
+++ src/main/java/Talismans/items/ModItems.java
@@ -2,6 +2,7 @@ package Talismans.items;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
@@ -13,15 +14,15 @@ public class ModItems {
 	public static void init(){
 		Talisman = new ItemTalisman();
 		GameRegistry.registerItem(Talisman, "Talisman");
-		
-		
-		
+	
 	}
 	public static void initItemRecipes(){
-		GameRegistry.addShapedRecipe(new ItemStack(ModItems.Talisman, 1,2), new Object[]{
-			"ZZZ", "ZQZ", "ZZZ", 'Z', Blocks.diamond_block, 'Q', Blocks.emerald_block
-		});
-
-
-}
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.Talisman, 2, 1), new Object[] {new ItemStack(ModItems.Talisman,1,1), ModItems.Talisman});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.Talisman, 2, 2), new Object[] {new ItemStack(ModItems.Talisman,1,2), ModItems.Talisman});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.Talisman, 2, 3), new Object[] {new ItemStack(ModItems.Talisman,1,3), ModItems.Talisman});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.Talisman, 2, 4), new Object[] {new ItemStack(ModItems.Talisman,1,4), ModItems.Talisman});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.Talisman, 2, 5), new Object[] {new ItemStack(ModItems.Talisman,1,5), ModItems.Talisman});
+		
+      }
+	
 }
