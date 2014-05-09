eb9ae6b6ae6417a24847847b7d06d6f18339177b
Travis Nicholls <thegigabit101@gmail.com>
2014-05-10 00:40:00 +0100

    Updated forge, Updated Baubles Now OUT OF BETA

--- src/main/java/Talismans/lib/Modinfo.java
+++ src/main/java/Talismans/lib/Modinfo.java
@@ -4,7 +4,7 @@ public class Modinfo {
 	
 	public static final String ID = "Talismans";
 	public static final String NAME = "Talismans";
-	public static final String Version = "1.0";
+	public static final String Version = "1.0.10";
 	public static final String SERVER_PROXY_CLASS = "Talismans.proxies.CommonProxy";
 	public static final String CLIENT_PROXY_CLASS = "Talismans.proxies.ClientProxy";
 	
